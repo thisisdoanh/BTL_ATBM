2b4f595407ad41c248008203b0302696c360c8e0
thisisdoanh <tranquocdoanh@gmail.com>
2023-06-27 22:47:54 +0700

    BTL

---
+++ WinFormsApp7/RichTextBoxExtend.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace RichTextBoxEx
+{
+    public class RichTextBoxWithShapes : RichTextBox
+    {
+        public void AddShape(Word.Shape shape)
+        {
+            // Tạo một InlineShape từ shape
+            Word.InlineShape inlineShape = shape.ConvertToInlineShape();
+
+            // Lấy hình ảnh của InlineShape và chuyển đổi nó thành một Bitmap
+            Word.Range range = inlineShape.Range;
+            range.CopyAsPicture();
+            Image image = Clipboard.GetImage();
+
+            // Chuyển đổi Bitmap thành một MemoryStream
+            using (System.IO.MemoryStream stream = new System.IO.MemoryStream())
+            {
+                image.Save(stream, ImageFormat.Png);
+
+                // Tạo một PictureBox để hiển thị hình ảnh
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Image = image;
+
+                // Thêm PictureBox vào RichTextBox
+                this.Controls.Add(pictureBox);
+            }
+        }
+    }
+}
